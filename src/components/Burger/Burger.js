9fc7c240ccf3004b379369341332dd8ed7957851
Maciej Rusek <m_rusek@vp.pl>
2017-11-27 22:17:54 +0100

    changed the way for create Burger to dynamic and generic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import React from 'react';
 import classes from './Burger.css';
 
 const burger = (props) => {
+    const transformedIngredietns = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredietns}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
