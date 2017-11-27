feb333e776a719196245ff903310bb28b3e7c787
Maciej Rusek <m_rusek@vp.pl>
2017-11-27 22:24:49 +0100

    added ingredients value validation

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import React from 'react';
 import classes from './Burger.css';
 
 const burger = (props) => {
-    const transformedIngredietns = Object.keys(props.ingredients)
+    let transformedIngredietns = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        });
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if(transformedIngredietns.length === 0) {
+        transformedIngredietns = <p>Please start adding ingredients</p>
+    }
 
     return (
         <div className={classes.Burger}>
